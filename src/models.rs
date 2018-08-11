9b0d1d455f83de4a1eae6e33f20c864ad2f585f3
h4llow3En <mail@felixdoering.com>
2018-08-11 14:31:32 +0200

    Add apple music regex match on ::new()

--- src/models.rs
+++ src/models.rs
@@ -33,7 +33,7 @@ impl SpotifyUrl {
     pub fn new(url: &str) -> APIResult<SpotifyUrl> {
         lazy_static! {
             static ref SPOTIFY: Regex = Regex::new(
-                r"https://open.spotify.com/(?:embed/)?user/(?P<user>[^/]+)/playlist/(?P<id>.+)"
+                r"https://open\.spotify\.com/(?:embed/)?user/(?P<user>[^/]+)/playlist/(?P<id>.+)"
             ).unwrap();
         }
         ensure!(SPOTIFY.is_match(url), ValidationError::NoValidSpoityUrl);
@@ -46,20 +46,41 @@ impl SpotifyUrl {
     }
 }
 
+impl AmusicUrl {
+    pub fn new(url: &str) -> APIResult<AmusicUrl> {
+        lazy_static! {
+            static ref AMUSIC: Regex = Regex::new(
+                r"https://(?:itunes\.apple|tools\.applemusic)\.com/(?:.{2}|embed/v1)/playlist/(?:(?P<name>[^/]*)/?)(?P<id>pl\.u-.+)"
+            ).unwrap();
+        }
+        ensure!(AMUSIC.is_match(url), ValidationError::NoValidAmusicUrl);
+
+        let capture = AMUSIC.captures(url).unwrap();
+        Ok(AmusicUrl {
+            link_name: capture["name"].to_string(),
+            id: capture["id"].to_string(),
+        })
+    }
+}
+
 #[cfg(test)]
 mod tests {
     use super::*;
 
     #[test]
     fn spotify_url() {
-        let playlist = SpotifyUrl::new("https://open.spotify.com/user/marauderxtreme/playlist/6YZJnIXDOHyY0eu6PEFLUQ").unwrap();
+        let playlist = SpotifyUrl::new(
+            "https://open.spotify.com/user/marauderxtreme/playlist/6YZJnIXDOHyY0eu6PEFLUQ",
+        ).unwrap();
         assert_eq!(playlist.id, String::from("6YZJnIXDOHyY0eu6PEFLUQ"));
         assert_eq!(playlist.user, String::from("marauderxtreme"));
     }
 
     #[test]
     fn spotify_url_embedded() {
-        let playlist = SpotifyUrl::new("https://open.spotify.com/embed/user/marauderxtreme/playlist/6YZJnIXDOHyY0eu6PEFLUQ").unwrap();
+        let playlist = SpotifyUrl::new(
+            "https://open.spotify.com/embed/user/marauderxtreme/playlist/6YZJnIXDOHyY0eu6PEFLUQ",
+        ).unwrap();
         assert_eq!(playlist.id, String::from("6YZJnIXDOHyY0eu6PEFLUQ"));
         assert_eq!(playlist.user, String::from("marauderxtreme"));
     }
@@ -67,7 +88,30 @@ mod tests {
     #[test]
     #[should_panic]
     fn spotify_wrong_url() {
-        let playlist = SpotifyUrl::new("https://open.spotify.com/wrong/url/pattern").unwrap();
+        let _ = SpotifyUrl::new("https://open.spotify.com/wrong/url/pattern").unwrap();
+    }
 
+        #[test]
+    fn amusic_url() {
+        let playlist = AmusicUrl::new(
+            "https://itunes.apple.com/de/playlist/mafia-ii-empire-central-radio/pl.u-WBYGFvpeKLk",
+        ).unwrap();
+        assert_eq!(playlist.id, String::from("pl.u-WBYGFvpeKLk"));
+        assert_eq!(playlist.link_name, String::from("mafia-ii-empire-central-radio"));
+    }
+
+    #[test]
+    fn amusic_url_embedded() {
+        let playlist = AmusicUrl::new(
+            "https://tools.applemusic.com/embed/v1/playlist/pl.u-WBYGFvpeKLk",
+        ).unwrap();
+        assert_eq!(playlist.id, String::from("pl.u-WBYGFvpeKLk"));
+        assert_eq!(playlist.link_name, String::from(""));
+    }
+
+    #[test]
+    #[should_panic]
+    fn amusic_wrong_url() {
+        let _ = AmusicUrl::new("https://tools.applemusic.com/embed/v1/noplaylist/pl.u-WBYGFvpeKLk").unwrap();
     }
 }
