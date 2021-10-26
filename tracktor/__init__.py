17d1ade4ced61edfaa5abdf6e61182533aa1cce8
h4llow3En <development@felixdoering.com>
2021-10-26 20:50:06 +0200

    Add CORS and mysql example in docker-compose

--- tracktor/__init__.py
+++ tracktor/__init__.py
@@ -2,12 +2,27 @@
 Main module for tracktor api
 """
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 import tracktor.sql
+from tracktor.config import config
 from tracktor.routers import admin, auth, version
 from tracktor.utils.startup import app_startup, app_shutdown
 
 app = FastAPI()
+
+if config.CORS_DOMAIN:
+    app.add_middleware(
+        CORSMiddleware,
+        allow_origins=[
+            f"http://{config.CORS_DOMAIN}",
+            f"https://{config.CORS_DOMAIN}"
+        ],
+        allow_credentials=True,
+        allow_methods=["*"],
+        allow_headers=["*"],
+    )
+
 app.include_router(admin.router)
 app.include_router(auth.router)
 app.include_router(version.router)
--- tracktor/config.py
+++ tracktor/config.py
@@ -13,16 +13,17 @@ class Config:  # pylint: disable=too-few-public-methods
     Config object for tracktor
     """
     SECRET_KEY = os.environ.get(
-        'SECRET_KEY',
-        default='565e1e6d786028a24fb1ff06cbae8f13bc96fdcdeff63fd90321d90ca2839fd7')
+        "SECRET_KEY",
+        default="565e1e6d786028a24fb1ff06cbae8f13bc96fdcdeff63fd90321d90ca2839fd7")
     ALGORITHM = "HS256"
     SQLALCHEMY_DATABASE_URI = os.environ.get(
-        'DATABASE_URL',
-        default='sqlite:///' + os.path.join(basedir, '../tracktor.db'))
-    ADMIN_USER = os.environ.get('ADMIN_USER', default='admin')
-    ADMIN_PASSWORD = os.environ.get('ADMIN_PASSWORD', default='password')
+        "DATABASE_URL",
+        default="sqlite:///" + os.path.join(basedir, "../tracktor.db"))
+    ADMIN_USER = os.environ.get("ADMIN_USER", default="admin")
+    ADMIN_PASSWORD = os.environ.get("ADMIN_PASSWORD", default="password")
     ACCESS_TOKEN_EXPIRE_MINUTES = 30
     OAUTH2_SCHEME = OAuth2PasswordBearer(tokenUrl="login")
+    CORS_DOMAIN = os.environ.get("CORS_DOMAIN", default=None)
 
 
 config = Config()
